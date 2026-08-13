765070396e2c4725bb8b8bb5eb784d7b6f81029c
BrRw-kurrid <kurrid@gmail.com>
2026-08-13 16:21:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // BrickRewind service worker
 // Verhoog CACHE_VERSION elke keer dat je index.html aanpast en opnieuw upload,
 // anders blijft de tablet de oude versie uit de cache tonen.
-const CACHE_VERSION = 'brickrewind-v0.18-17';
+const CACHE_VERSION = 'brickrewind-v0.18-18';
 const APP_SHELL = [
   './',
   './index.html',
